e1d3ff3b9ffd6c1fcaf54cd0ec1ad3b64edb763d
Ishaan Gupta <15ishaangupta@gmail.com>
2020-12-11 00:46:29 +0530

    details form and profile completed

---
+++ project/src/main/java/com/ishaan/project/controller/UserDetailsController.java
@@ -0,0 +1,35 @@
+package com.ishaan.project.controller;
+
+import com.ishaan.project.model.User;
+import com.ishaan.project.model.UserDetailsRequest;
+import com.ishaan.project.repository.RegistrationRepository;
+import com.ishaan.project.service.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserDetailsController {
+
+    @Autowired
+    private RegistrationService service;
+
+    @Autowired
+    private RegistrationRepository registrationRepo;
+
+    @PostMapping("/UserDetails")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    public User userDetails(@RequestBody UserDetailsRequest userDetailsRequest) {
+        User user = service.fetchUserByUsername(userDetailsRequest.getUsername());
+        user.setCollegeName(userDetailsRequest.getCollegeName());
+        user.setBranch(userDetailsRequest.getBranch());
+        user.setSem(userDetailsRequest.getSem());
+        user.setGender(userDetailsRequest.getGender());
+        user.setBirthdate(userDetailsRequest.getBirthdate());
+        user.setBio(userDetailsRequest.getBio());
+        registrationRepo.save(user);
+        return user;
+    }
+}
--- project/src/main/java/com/ishaan/project/repository/RegistrationRepository.java
+++ project/src/main/java/com/ishaan/project/repository/RegistrationRepository.java
@@ -2,6 +2,10 @@ package com.ishaan.project.repository;
 
 import com.ishaan.project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
 
 public interface RegistrationRepository extends JpaRepository<User, Integer> {
 
